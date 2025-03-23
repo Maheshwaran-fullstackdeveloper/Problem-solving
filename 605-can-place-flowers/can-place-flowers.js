82372987c0417c55e4302adfd9614f41e435c39f
Maheshwaran G <145131503+Maheshwaran-fullstackdeveloper@users.noreply.github.com>
2025-03-23 09:47:35 +0530

    Time: 0 ms (100.00%) | Memory: 58.1 MB (7.27%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.js
+++ 605-can-place-flowers/can-place-flowers.js
@@ -5,15 +5,13 @@
  */
 var canPlaceFlowers = function(flowerbed, n) {
     const l = flowerbed.length;
-    
-    for (let i = 0; i < l; i++) {
-        if (flowerbed[i] === 0 &&
-            (i === 0 || flowerbed[i-1] === 0) &&
-            (i === l - 1 || flowerbed[i+1] === 0)) {
+
+    for(i=0; i<l; i++) {
+        if(flowerbed[i]===0 && (i===0 || flowerbed[i-1]===0) && (i===l-1 || flowerbed[i+1]===0)) {
             flowerbed[i] = 1;
             n--;
-            if (n === 0) { return true; }
         }
+        if(n===0) return true
     }
-    return n <= 0;    
+    return n<=0
 };
