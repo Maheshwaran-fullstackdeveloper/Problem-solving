aa93e6ac35581b5fe395079961510b45faa7b804
Maheshwaran G <145131503+Maheshwaran-fullstackdeveloper@users.noreply.github.com>
2025-04-20 19:15:56 +0530

    Time: 1 ms (82.08%) | Memory: 55.1 MB (44.02%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -6,11 +6,5 @@
 var intersection = function(nums1, nums2) {
     const num1 = new Set(nums1);
     const num2 = new Set(nums2);
-    let result = []
-    for(num of num1) {
-        if(num2.has(num)) {
-            result.push(num)
-        }
-    }
-    return result
+    return Array.from(num1.intersection(num2))
 };
